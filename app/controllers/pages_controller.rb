328ad820d7ab5e3781b862ada23ef0b07be1969c
Anna Bendahan <an.bendahan@gmail.com>
2018-09-15 08:14:53 +0200

    navbar fixed

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,14 +1,7 @@
 class PagesController < ApplicationController
   def home
   end
-
-  def chalet
-  end
-
-  def histoire
-  end
-
-  def alentours 
-  end
-  
+  def download_pdf
+  send_file "#{Rails.root}/app/assets/images/CV AOUT 2018.pdf", type: "application/pdf", x_sendfile: true
+end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
+
   root to: 'pages#home'
-  get '/chalet', to: 'pages#chalet'
-  get '/histoire', to: 'pages#histoire'
-  get'/alentours', to: 'pages#alentours'
+
+  get 'download_pdf', to: "pages#download_pdf"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
