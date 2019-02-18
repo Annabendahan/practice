9302f5d090c21abcfbf51725a63cab8deb6d62bb
Anna Bendahan <an.bendahan@gmail.com>
2019-02-18 13:08:32 +0100

    CV updated

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,6 +2,6 @@ class PagesController < ApplicationController
   def home
   end
   def download_pdf
-  send_file "#{Rails.root}/app/assets/images/CV AOUT 2018.pdf", type: "application/pdf", x_sendfile: true
+  send_file "#{Rails.root}/app/assets/images/CVFEB2019-3.pdf", type: "application/pdf", x_sendfile: true
 end
 end
